4ad13e31b036cb730c0c194984221b1febb5a29f
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-12 20:01:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DoctorCare
+ One Page com tema de medicina, com objetivo de divulgar serviços e tratamentos médicos, layout único e concentração das informações em um mesmo ambiente. O objetivo principal é trazer dinamismo e agilidade para a navegação do usuário.
---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+window.addEventListener('scroll' , onScroll)
+onScroll ()
+
+function onScroll () {
+  navOnScroll ()
+  btnOnScroll ()
+  activeMenu (home)
+  activeMenu (services)
+  activeMenu (about)
+  activeMenu (contact)
+}
+
+function activeMenu (section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const checkingLine =  targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight
+  const passedTargetLine = sectionEndsAt <= targetLine
+
+  // limites da seção 
+
+  const sectionBoundaries = checkingLine && !passedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  
+  menuElement.classList.remove('active')
+  if (sectionBoundaries){
+      menuElement.classList.add('active')
+  }
+
+} // aula 5 / 2hr 25min.
+
+function navOnScroll (){
+  if(scrollY > 0 ) {
+    navigation.classList.add('scroll')
+  }
+  else {
+    navigation.classList.remove('scroll')
+  }
+ 
+}
+
+function btnOnScroll () {
+  if(scrollY > 700 ) {
+    backToTopBtn.classList.add('show')
+  }
+  else {
+    backToTopBtn.classList.remove('show')
+  }
+
+}
+
+function openMenu () {
+  document.body.classList.add ('menu-expanded')
+}
+
+function closeMenu () {
+  document.body.classList.remove ('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top' ,
+  distance: '30px' ,
+  duration: 700,
+}).reveal(
+ `#home, 
+  #home img, 
+  #home .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content
+  `);
